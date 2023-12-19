1ffddd8090c1854f2fa3b8e57da70ebc66ee8d20
Anil kumar nayak <anilkumarnayak639@gmail.com>
2023-12-19 12:47:33 +0530

    signup page updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./Pages/SignUp";
+import Login from "./Pages/Login";
+import Chat from "./Pages/Chat";
 function App() {
   return (
     <div className="App">
-      <h1>anil</h1>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
     </div>
   );
 }
