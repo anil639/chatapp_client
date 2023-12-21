431d187ee38bd34652f1405f2ac71dab11539fe6
Anil kumar nayak <anilkumarnayak639@gmail.com>
2023-12-21 17:30:44 +0530

    set avatar updated.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import SignUp from "./Pages/SignUp";
 import Login from "./Pages/Login";
 import Chat from "./Pages/Chat";
+import SetAvatar from "./Components/SetAvatar";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Chat />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
       </Routes>
     </div>
   );
